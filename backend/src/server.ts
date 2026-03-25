b7079e90d4590478d2ba0cefbee4f231f5d19af0
vlaier <piotrzielu.1996@gmail.com>
2026-03-25 10:47:11 +0100

    feat: add CORS support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { state } from "./state/state";
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/resort", (req, res) => {
   res.json(state.getResortLayout());
